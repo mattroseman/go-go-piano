d84642136ff669f47e1ace34bcdccd557aff3ef1
Cameron Sinko <csinko@gmail.com>
2017-01-14 19:38:53 -0500

    Added midiutil library example with 1/16 notes

--- python/main.py
+++ python/main.py
@@ -1,1 +1,22 @@
-# Create Sheet music from piano data`
+
+from midiutil.MidiFile import MIDIFile
+
+degrees  = [60, 62, 64, 65, 67, 69, 71, 72] # MIDI note number
+track    = 0
+channel  = 0
+time     = 0.00   # In beats
+duration = 0.25   # In beats
+tempo    = 60  # In BPM
+volume   = 100 # 0-127, as per the MIDI standard
+
+MyMIDI = MIDIFile(1) # One track, defaults to format 1 (tempo track
+                     # automatically created)
+MyMIDI.addTempo(track,time, tempo)
+
+for pitch in degrees:
+    MyMIDI.addNote(track, channel, pitch, time, duration, volume)
+    time = time + 0.25
+
+with open("major-scale.mid", "wb") as output_file:
+    MyMIDI.writeFile(output_file)
+
